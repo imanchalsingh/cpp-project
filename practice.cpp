1514c0281d507a95fd3f8e2c95de549378943b0f
anchalDev159357 <singhanchal159357@gmail.com>
2025-04-17 18:49:59 +0530

    added changes

--- practice.cpp
+++ practice.cpp
@@ -35,16 +35,33 @@ int main()
         cout << b;
     }
     // 3. Calculate Factorial of a number N.
-    int fact, numN, count;
+    int fact = 1, numN, count;
+
     cout << "\nEnter a number: ";
     cin >> numN;
-    fact = 1;
-    for (count = 1; count <= numN; count++)
-    {
-        fact = fact * count;
-    }
 
+    if (numN < 0)
+    {
+        cout << "Factorial of a negative number is not defined." << endl;
+    }
+    else
+    {
+        for (count = 1; count <= numN; count++)
+        {
+            fact = fact * count;
+        }
+        cout << "Factorial of " << numN << " is: " << fact << endl;
+    }
+    // 4. Given a person's age, find if they should get a driving license or not.
+    int age;
+    cout << "\nEnter your age: ";
+    cin >> age;
+    if (age>18)
+    {
+        cout << "They should get a driving license.";
+    }
+    else {
+        cout << "No, they should not get a license.";
+    }
     return 0;
 }
-
-// 4. Given a person's age, find if they should get a driving license or not.
